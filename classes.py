2f0a212f08dcfa7e4efd0a5f5e1ee0acf2abbea2
Erasuithon2000 <mikeroditis2000@gmail.com>
2021-12-25 10:06:01 +0200

    pVp can now drag pieces

--- classes.py
+++ classes.py
@@ -1,8 +1,9 @@
 class Board:
     def __init__(self):
         self.state = None
-        self.available_X = 9
-        self.available_0 = 9
+        self.Xs_on_board = 0
+        self.Os_on_board = 0
+        self.clear = '∙'
         self.second = 'X'
         self.first = '●'
         self.board = []
@@ -37,8 +38,8 @@ class Board:
 
     def initialize(self):
         self.state = '0' * 24
-        self.available_X = 9
-        self.available_0 = 9
+        self.Xs_on_board = 0
+        self.Os_on_board = 0
         for _ in range(self.side_length):
             row = []
             for _ in range(self.side_length):
@@ -72,7 +73,7 @@ class Board:
                 self.board[i][j] = ' '
 
         for key, (x, y) in self.playable_pos.items():
-            self.board[x][y] = '∙'
+            self.board[x][y] = self.clear
 
     def show(self):
         for i in range(self.side_length):
@@ -84,24 +85,106 @@ class Board:
         x, y = self.playable_pos[key]
         if is_first:
             self.board[x][y] = self.first
-            self.available_0 -= 1
+            self.Os_on_board += 1
             val_for_state = 1
         else:
             self.board[x][y] = self.second
-            self.available_X -= 1
+            self.Xs_on_board += 1
             val_for_state = 2
 
         self.state = f"{self.state[:key]}{val_for_state}{self.state[key + 1:]}"
 
-    def player_move(self, played, is_first=True):
+    def drag_piece(self, key1, key2, is_first=True):
+        x1, y1 = self.playable_pos[key1]
+        x2, y2 = self.playable_pos[key2]
+        self.board[x1][y1] = self.clear
+        if is_first:
+            self.board[x2][y2] = self.first
+            val_for_state = 1
+        else:
+            self.board[x2][y2] = self.second
+            val_for_state = 1
+
+        small = min(key1, key2)
+        small_val = 0 if small == key1 else val_for_state
+        big = max(key1, key2)
+        big_val = val_for_state - small_val
+        self.state = f"{self.state[:small]}{small_val}{self.state[small + 1:big]}{big_val}{self.state[big + 1:]}"
+
+    def is_next(self, key1, key2):
+        neighbors = {
+            0: (1, 9),
+            1: (0, 2, 4),
+            2: (1, 14),
+            3: (4, 10),
+            4: (3, 5, 1, 7),
+            5: (6, 13),
+            6: (7, 11),
+            7: (6, 8, 4),
+            8: (7, 12),
+            9: (0, 10, 21),
+            10: (3, 9, 11, 18),
+            11: (10, 6, 15),
+            12: (8, 13, 17),
+            13: (12, 5, 14, 20),
+            14: (13, 2, 23),
+            15: (11, 16),
+            16: (15, 17, 19),
+            17: (16, 12),
+            18: (10, 19),
+            19: (18, 20, 16, 22),
+            20: (19, 13),
+            21: (9, 22),
+            22: (21, 23, 19),
+            23: (22, 14)
+        }
+        return key2 in neighbors[key1]
+
+    def draggable(self, key1, key2, is_first=True):
+        x1, y1 = self.playable_pos[key1]
+        x2, y2 = self.playable_pos[key2]
+        if not self.is_next(key1, key2):
+            return False
+        if is_first:
+            if self.board[x1][y1] != self.first:
+                return False
+        elif self.board[x1][y1] != self.second:
+            return False
+        if self.board[x2][y2] != self.clear:
+            return False
+        return True
+
+    def enter_move(self, played, is_first=True, is_drag=False):
         string = "first's turn: " if is_first else "Second's turn: "
         key = input(string)
-        while not key.isnumeric() or not 0 <= int(key) <= 23 or int(key) in played:
-            key = input('Enter a valid number in the range [0, 23]: ')
-        key = int(key)
+        if not is_drag:
+            while not key.isnumeric() or not 0 <= int(key) <= 23 or int(key) in played:
+                key = input('Enter a valid number in the range [0, 23]: ')
+        else:
+            while not key.isnumeric() or not 0 <= int(key) <= 23:
+                key = input('Enter a valid number in the range [0, 23]: ')
+        return int(key)
+
+    def enter_drag(self, played, is_first=True):
+        while True:
+            print('Drag Piece From')
+            key1 = self.enter_move(played, is_first=is_first, is_drag=True)
+            print('Drag Piece To')
+            key2 = self.enter_move(played, is_first=is_first, is_drag=True)
+            if self.draggable(key1, key2, is_first=is_first):
+                break
+            print('Please enter valid input')
+
+        self.drag_piece(key1, key2, is_first=is_first)
+        played.remove(key1)
+        played.add(key2)
+        return played
+
+    def player_move(self, played, is_first=True):
+        key = self.enter_move(played, is_first)
         self.make_the_move(key, is_first)
 
-        played.append(key)
+        played.add(key)
         return played
 
     def computer_move(self, played, is_first=True):
@@ -116,27 +199,33 @@ class Board:
         return played
 
     def pvp(self):
-        played = []
-        while not self.available_X == self.available_0 == 0:
+        played = set()
+        while not self.Xs_on_board == self.Os_on_board == 9:
             played = self.player_move(played)
             self.show()
 
             played = self.player_move(played, is_first=False)
             self.show()
+        while True:
+            played = self.enter_drag(played)
+            self.show()
+
+            played = self.enter_drag(played, is_first=False)
+            self.show()
 
     def pvc(self, is_computer_first=False):
-        played = []
+        played = set()
         is_player_first = True
         if is_computer_first:
             is_player_first = False
             self.computer_move(played)
             self.show()
 
-        while not self.available_X == self.available_0 == 0:
+        while not self.Xs_on_board == self.Os_on_board == 9:
             played = self.player_move(played, is_player_first)
             self.show()
 
-            if self.available_X == self.available_0 == 9:
+            if self.Xs_on_board == self.Os_on_board == 9:
                 break
             print("Computer's turn: ")
 
